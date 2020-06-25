c5989d6ecb4fe5cdb68997c3218adb42dcfde02a
Eduardo-H <edu-henrique2014@hotmail.com>
2020-06-25 20:09:05 -0300

    Models migrated and added to admin page

--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Alocacao)
+admin.site.register(Recolhimento)
+admin.site.register(AlocacaoRecolhimento)
+admin.site.register(AlocacaoAgente)
+admin.site.register(RecolhimentoAgente)
+admin.site.register(Agente)
+admin.site.register(Viatura)
+admin.site.register(Item)
+admin.site.register(Estoque)
+admin.site.register(ItemPerdidoExtraviado)
--- management/models.py
+++ management/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Viatura(models.Model):
+    numero = models.IntegerField()
+    placa = models.CharField(max_length=7)
+
+class Item(models.Model):
+    nome = models.CharField(max_length=100)
+
+class Agente(models.Model):
+    SEXO_CHOICES = (
+        ('Feminino', 'Feminino'),
+        ('Masculino', 'Masculino')
+    )
+
+    nome = models.CharField(max_length=200)
+    datanascimento = models.DateField()
+    sexo = models.CharField(max_length=10, choices=SEXO_CHOICES)
+    gritodeduerra = models.CharField(max_length=50)
+
 class Alocacao(models.Model):
     TURNO_CHOICES = (
         ('Manhã', 'Manhã'),
@@ -11,61 +30,45 @@ class Alocacao(models.Model):
 
     data = models.DateField()
     horario = models.TimeField()
-    item = models.ForeignKey(Item)
+    item = models.ForeignKey(Item, on_delete=models.PROTECT)
     quantidade = models.IntegerField()
     turno = models.CharField(max_length=10, choices=TURNO_CHOICES)
-    viatura = models.ForeignKey(Viatura, blank=True)
+    viatura = models.ForeignKey(Viatura, on_delete=models.PROTECT, blank=True)
     local = models.CharField(max_length=250)
     motivo = models.CharField(max_length=100, blank=True)
-    cadastrador = models.ForeignKey(User)
+    cadastrador = models.ForeignKey(User, on_delete=models.PROTECT)
 
     def __str__(self):
         return 'Alocação feita no dia' + self.data + ' em ' + self.local
 
 class AlocacaoAgente(models.Model):
-    alocacao = models.ForeignKey(Alocacao)
-    agente = models.ForeignKey(Agente)
+    alocacao = models.ForeignKey(Alocacao, on_delete=models.PROTECT)
+    agente = models.ForeignKey(Agente, on_delete=models.PROTECT)
 
 class Recolhimento(models.Model):
     data = models.DateField()
     horario = models.TimeField()
-    alocacao = models.ForeignKey(Alocacao)
-    cadastrador = models.ForeignKey(User)
+    alocacao = models.ForeignKey(Alocacao, on_delete=models.PROTECT)
+    cadastrador = models.ForeignKey(User, on_delete=models.PROTECT)
 
-class RecolhimentoAgente(models.Models):
-    recolhimento = models.ForeignKey(Recolhimento)
-    agente = models.ForeignKey(Agente)
+class RecolhimentoAgente(models.Model):
+    recolhimento = models.ForeignKey(Recolhimento, on_delete=models.PROTECT)
+    agente = models.ForeignKey(Agente, on_delete=models.PROTECT)
 
 class AlocacaoRecolhimento(models.Model):
     data = models.DateField()
     horario = models.TimeField()
-    alocacao = models.ForeignKey(Alocacao, blank=True)
-    recolhimento = models.ForeignKey(Recolhimento, blank=True)
+    alocacao = models.ForeignKey(Alocacao, on_delete=models.PROTECT, blank=True)
+    recolhimento = models.ForeignKey(Recolhimento, on_delete=models.PROTECT, blank=True)
 
-class Item(models.Model):
-    nome = models.CharField(max_length=100)
+
 
 class Estoque(models.Model):
-    item = models.ForeignKey(Item)
+    item = models.ForeignKey(Item, on_delete=models.PROTECT)
     quantidade = models.IntegerField()
 
 class ItemPerdidoExtraviado(models.Model):
     data = models.DateField()
     horario = models.TimeField()
-    alocacao = models.ForeignKey(Alocacao)
+    alocacao = models.ForeignKey(Alocacao, on_delete=models.PROTECT)
     quantidade = models.IntegerField()
-
-class Agente(models.Model):
-    SEXO_CHOICES = (
-        ('Feminino', 'Feminino'),
-        ('Masculino', 'Masculino')
-    )
-
-    nome = models.CharField(max_length=200)
-    datanascimento = models.DateField()
-    sexo = models.CharField(max_length=8, choices=SEXO_CHOICES)
-    gritodeduerra = models.CharField(max_length=50)
-
-class Viatura(models.Model):
-    numero = models.IntegerField()
-    placa = models.CharField(max_length=7)
